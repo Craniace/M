0b63ffb4b7395087dfff6e4e80aa81811c2b03cd
Priyanshu Saxena <priyanshusaxena0420@gmail.com>
2022-08-16 17:07:50 +0530

    home+login+signup+forgotpassword

--- app/src/main/java/com/example/mtxo/login.java
+++ app/src/main/java/com/example/mtxo/login.java
@@ -2,13 +2,24 @@ package com.example.mtxo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+    private Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        button4=findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(login.this,forgotpassword.class);
+                startActivity(intent);
+}       });
     }
 }
--- app/src/main/java/com/example/mtxo/signup.java
+++ app/src/main/java/com/example/mtxo/signup.java
@@ -2,14 +2,26 @@ package com.example.mtxo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class signup extends AppCompatActivity {
+    private Button button8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+
+        button8=findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(signup.this, login.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
